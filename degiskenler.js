1b147902a80701eb0fd62c1158ea3c9186ccfa52
betuskoksal <koksal.betus@gmail.com>
2024-01-19 19:24:49 +0300

    update

---
+++ degiskenler.js
@@ -0,0 +1,63 @@
+/*
+-DEĞİŞKEN TANIMLAMA-
+var name="Fikret";
+
+-Değişkeni tarayıcıda tanımlamak için 3 yol-
+1) console.log(Name);
+2) alert(Name);
+3)document.write(Name);
+
+*/
+
+var name="Fikret";
+console.log(Name);
+Name="Baki";
+console.log(Name);
+var surname="Çevik";
+console.log(surname);
+
+/*
+-JAVA SCRİPT EKRANA YAZDIRMA YÖNTEMLERİ
+
+1-HTML ekranlarına yazdırma (innerHTML)
+2-HTML sayfasına yazdırma  (document.write)
+3-Açılır uyarı penceresine yazdırma (alert())
+4-Browser konsoluna yazdırma (console.log())
+
+
+DEĞİŞKEN TANIMLAMA KURALALLARI
+-Türkçe  karakter kullanılamaz
+-Sayı ile başlamaz
+-Sembol olark _ ve $ kullanılır
+-Camel Case: ogrenciAd, userName, notDurum
+-Pascal Case: OgrenciAd, UserName, NotDurum
+-Snakecase: ogrenci_ad, user_name, not_durum
+
+*/
+
+
+/* 
+-var değişkeninin özelliği-
+
+-Değişken tanımlanırken "var, let, const" anahtar kelimelerinden biri kullanılır
+-var anahtar kelimesiyle tanımlanan değişkenler, fonksiyonlar
+içinde ve ya dışında tanımlanabilir.
+-Değişken değeri her zaman değiştirilebilir.
+
+*/
+
+/* -Yan yana yazdırma- */
+
+var Name="betül";
+var surname="köksal";
+console.log(Name+surname);
+
+/* -Araya boşluk bırakma- */
+
+console.log(Name+" "+surname);
+
+
+
+
+
+
