81dac6a5adcdc02676b495e5f5c2939e0e282c9f
betuskoksal <koksal.betus@gmail.com>
2024-01-19 19:46:35 +0300

    update

--- degiskenler.js
+++ degiskenler.js
@@ -57,7 +57,59 @@ console.log(Name+surname);
 console.log(Name+" "+surname);
 
 
+/* DEĞİŞKEN TÜRLERİ
+1-NUMBER
+var x=10;
 
+2-STRING
+var name="Betül";
+
+3-BOOLEAN
+var sinavNotu=40;
+var  basailimi=(sinavNotu>=50);
+
+4-UNDEFINED
+Değer atanmamış bir değişkeni temsil eder.
+
+var yas;
+console.log(yas);
+yas değişkeni tanımlayıp içine bir değer atamazsak sonuc underfined olur  
+
+5-NULL
+Bilinçli olarak boş bir değer atanmış değişkeni temsil eder.
+
+var x=null;
+
+6-OBJECT(NESNE)
+Özellikler ve değerlerin birleşimidir.
+Toplu veri tutmamıza  yarayan veri tipidir.
+
+var ogrenci={ };
+
+7-ARRAY(DİZİ)
+Bir diziye ait öğeleri  saklamak için kullanılır.
+
+var numbers=[1,2,3,4...];
+
+8-FUNCTION(FONKSİYON)
+Bir işlevi temsil eder.
+
+function gree (name)
+    {
+        console.log("merhaba, ${name}!");
+    }
+
+9-SYMBOL
+Benzersiz ve değiştirilemez veri türüdür.
+
+const id=Symbol("id");
+
+10-BIGLNT
+Uzun tam satyılar
+
+4567895676n
+
+*/
 
 
 
