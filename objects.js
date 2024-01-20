c2c4c65ea3a4895a8dcf420d7194e3f18ffdcc65
betuskoksal <koksal.betus@gmail.com>
2024-01-20 13:56:17 +0300

    update

--- objects.js
+++ objects.js
@@ -1,3 +1,100 @@
+/*  OBJECTS
+-Nesne değişken türüdür.
+-Nesneler birden fazla değer alabilir.
+    var araba={marka:"Fiat", model:"500", renk:"beyaz"};
+    var bilgiler={ ad:"Yusuf", soyad:"Sezer"};
+-Javascript nesne işleri nesne özelliklerinde function olarak saklar.
+-Anahtar kod verereko anahtar aracılığıyla hangi bilgiye ulaşacağımızı 
+ tanımlamış oluyoruz.
+-Obje içinde obje tanımlayabilriz.
+
+*/
+
+    var kullanici=
+    {
+        "adres":
+        {
+            "sehir":"Kocaeli",
+            "ilce":"İzmit",
+        }
+    }
+
+    var sonuc ;
+    sonuc=kullanici.adres.sehir;
+    sonuc=kullanici.adres.ilce;
+    console.log(sonuc);
+
+
+/*  OBJE İÇİNDE DİZİ TANIMLAMASI  */
+
+    var kullanici=
+    {
+        "adres":
+        {
+            "sehir":"Kocaeli",
+            "ilce":"İzmit",
+        },
+        "hobiler":["sinema","spor"]
+    }
+
+    var sonuc;
+    sonuc=kullanici.hobiler[0];
+    console.log(sonuc);
+
+
+/*  OBJE İÇİNDE DİZİ TANIMLAMASI  */
+
+var urunler=
+
+[
+    { 
+        "urun_adi":"Samsung s22",
+        "urun_fiyat":13000,
+    },
+    {
+        "urun_adi":"Samsung s23",
+        "urun_fiyat":15000,
+    }
+]
+
+sonuc=urunler[0].urun_adi;
+console.log(sonuc);
+
+
+/* DİZİLER VE OBJECTS(NESNELER) ARASINDAKİ FARK
+
+-Diziler sıra numarası kullanır.
+-Nesneler üye isimlerini kullanır.
+-Adlandırılmış dizileri desteklemez.
+-Üye isimleri ile erişmek istenirse nesneler kullanılbilir.
+-Değerlere sıra numarası ile erişmek istenirse diziler kullanıalbilir.
+
+*/
+
+
+/* UYGULAMA */
+
+var sayilar=[1,4,5,8,4,3];
+console.log(sayilar[0]+sayilar[1]+sayilar[2]+sayilar[3]+sayilar[4]+sayilar[5]);
+for (var i=0; i<sayilar.lenght; i++)
+    {
+        console.log(sayilar[i]);
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*  1-sipariş bilgierini object içerisinde saklayınız 
     2-Her siparişin ayrı ayrı kdv dahil toplam ödenen ücretini hesaplyınız (kdv %18)
 siparis id: 101
