4fa899e5bf5afa543e84621e3866504710850e13
betuskoksal <koksal.betus@gmail.com>
2024-01-31 00:37:25 +0300

    update

---
+++ onload.js
@@ -0,0 +1,23 @@
+/* onload nedir?
+-Sayfanın yüklenmesi sırasında belirli görevleri veya işlemleri gerçekleştirmek 
+için kullanılır. 
+
+-Bu görevler arasında JavaScript kodu çalıştırmak, belirli elemanları manipüle etmek, 
+harici kaynakları yüklemek gibi işlemler bulunabilir.
+
+*/
+
+window.onload=Yuklendi;
+
+/*
+Sayfadaki elemanlar yüklendikten sonra çalışmasını istiyorsak bunu kullanabiliriz.
+*/
+ 
+// ya da
+
+<body onload="Yuklendi()"> 
+
+</body> 
+
+
+
