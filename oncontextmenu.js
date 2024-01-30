80d606384d41da518600f6a77c85a6018224ff77
betuskoksal <koksal.betus@gmail.com>
2024-01-31 00:33:42 +0300

    update

---
+++ oncontextmenu.js
@@ -0,0 +1,14 @@
+/* oncontextmenu nedir ?
+
+-HTML'deki bir element üzerinde sağ tıklama (context menu) olayını
+yakalamak için kullanılan bir JavaScript olay özelliğidir. 
+
+-Sağ tıklama, bir kullanıcının bir öğe üzerine sağ tıkladığında tarayıcı tarafından 
+görüntülenen bağlam menüsünü tetikler.
+
+-Bu olay özelliği, bir elemente sağ tıklandığında belirli bir JavaScript kodunu 
+çalıştırmak için kullanılır. 
+
+-Örneğin, belirli bir sağ tıklama durumunda özel bir bağlam menüsü göstermek veya 
+özel bir işlem gerçekleştirmek için kullanılabilir.
+*/
