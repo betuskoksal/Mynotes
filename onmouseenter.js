8fab31b452ce18d0d4fc2f082971f0401839b43c
betuskoksal <koksal.betus@gmail.com>
2024-01-31 00:38:25 +0300

    update

---
+++ onmouseenter.js
@@ -0,0 +1,9 @@
+/* onmouseenter nedir?
+
+-Bir HTML elementinin üzerine fare ile gelindiğinde (mouse enter) çalışacak 
+JavaScript olay (event) özelliğini temsil eder. Bu olay, fare imleci belirli bir 
+HTML elementinin içine girdiğinde tetiklenir.
+
+-Herhangi bir nesnenin üzerine geldiğimizde bu olay tetiklenir.
+
+*/
