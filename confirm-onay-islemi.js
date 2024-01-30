3475392c7f15a525f872cb1ac70e235661bc6f47
betuskoksal <koksal.betus@gmail.com>
2024-01-31 00:43:43 +0300

    update

---
+++ confirm-onay-islemi.js
@@ -0,0 +1,13 @@
+// Sonuca göre işlemimizi devam ettirmemizi sağlayan bir fonksiyondur.
+
+/* 
+-Confirm fonksiyonu, JavaScript'te bir tarayıcı penceresinde kullanıcıya bir onay iletişim 
+kutusu (confirmation dialog) göstermek için kullanılır. Bu iletişim kutusu, 
+genellikle bir işlemi gerçekleştirmek isteyip istemediğini doğrulamak veya reddetmek 
+amacıyla kullanılır.
+
+-Confirm fonksiyonu bir boolean değer döndürür. Eğer kullanıcı "OK" (Tamam) düğmesine tıklarsa,
+ fonksiyon true değerini döner. Eğer "Cancel" (İptal) düğmesine tıklarsa, fonksiyon false 
+ değerini döner.
+
+*/
