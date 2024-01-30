25c5910b20b89b95075161acd03107bdbcaa920c
betuskoksal <koksal.betus@gmail.com>
2024-01-31 00:42:03 +0300

    update

---
+++ onmouseup-onmousedown.js
@@ -0,0 +1,28 @@
+/* onmouseup nedir?
+
+-Bir HTML öğesi üzerinde fare düğmesinin bırakılması (mouse up) durumunda gerçekleşen 
+bir etkinlik belirleyicisidir. Bu olay, kullanıcı fare düğmesini tıkladıktan sonra 
+düğmeyi bıraktığında tetiklenir.
+
+-Kullanıcının fare düğmesini bıraktığında çalıştırılır. 
+-Bu, özellikle bir butonun tıklama durumunu kontrol etmek veya kullanıcının 
+fare düğmesini bıraktığında başka bir eylem gerçekleştirmek için kullanılabilir.
+
+*/
+
+
+/* onmousedown nedir?
+
+-Bir HTML öğesi üzerinde fare düğmesinin basılması (mouse down) durumunda gerçekleşen 
+bir etkinlik belirleyicisidir. 
+
+-Bu etkinlik, kullanıcı fare düğmesine tıkladığında tetiklenir.
+
+-Genellikle, onmousedown etkinliği ile ilgili JavaScript kodları, 
+kullanıcının fare düğmesini basılı tuttuğu durumda çalıştırılır. 
+
+-Bu, özellikle bir butonun tıklama durumunu kontrol etmek, kullanıcının fare düğmesini 
+basılı tuttuğunda başka bir eylem gerçekleştirmek veya sürükle ve bırak gibi interaktif 
+özellikleri uygulamak için kullanılabilir.
+
+*/
