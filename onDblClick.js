7d0d4cd788692c2a3f06c2c6eba307dd916f17be
betuskoksal <koksal.betus@gmail.com>
2024-01-31 00:36:31 +0300

    update

---
+++ onDblClick.js
@@ -0,0 +1,8 @@
+/* onDblClick nedir?
+
+-HTML'deki bir elementin üzerine çift tıklandığında çalışacak olan 
+JavaScript olay özelliğini temsil eder. Bu özellik, kullanıcı bir HTML elementine 
+çift tıkladığında çalışacak JavaScript kodunu belirlemenizi sağlar.
+
+*/
+
