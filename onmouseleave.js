add48d6d22e422a2d54ba117e6ed6e756079ada2
betuskoksal <koksal.betus@gmail.com>
2024-01-31 00:39:17 +0300

    update

---
+++ onmouseleave.js
@@ -0,0 +1,11 @@
+/* onmouseleave nedir?
+
+-HTML ve JavaScript kullanılarak fare imlecinin belirli bir HTML elementinin 
+üzerinden ayrıldığında (mouse leave) tetiklenen bir JavaScript olayıdır.
+
+-Bu olay, kullanıcı fare imleci ile belirli bir HTML elementinin 
+üzerine gelirken (onmouseenter olayı), ardından bu elementin sınırları 
+dışına çıktığında tetiklenir. Yani, kullanıcı belirli bir elementin sınırları 
+içindeyken bu olay tetiklenmez, ancak sınırları dışına çıkınca tetiklenir.
+
+*/
