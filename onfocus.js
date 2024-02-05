e0b83222391177d0e0da81c619c6f50e7682e611
betuskoksal <koksal.betus@gmail.com>
2024-02-06 00:02:05 +0300

    update

---
+++ onfocus.js
@@ -0,0 +1,8 @@
+/* onfocus nedir?
+-onfocus, bir HTML öğesi belirli bir öğe üzerine odaklandığında 
+(tıklanarak veya klavye ile seçilerek) tetiklenen bir JavaScript olayıdır. 
+
+-Bu olay, genellikle bir giriş alanında veya diğer etkileşimli öğelerde kullanılır.
+
+*/
+
