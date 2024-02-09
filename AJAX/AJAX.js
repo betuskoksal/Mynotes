c4dd98b61c8e982878c9ba4cda5cbff8b3db809a
betuskoksal <koksal.betus@gmail.com>
2024-02-09 21:32:36 +0300

    update

---
+++ AJAX/AJAX.js
@@ -0,0 +1,30 @@
+/* AJAX (Asynchronous JavaScript and XML):
+
+-Ajax (Eşzamansız JavaScript and XML), web sayfalarında dinamik içerikleri 
+yüklemek ve göndermek için kullanılan bir web geliştirme teknolojisidir. 
+
+-Ajax, sayfa yeniden yüklenmeden web sayfalarının arka planda veri alışverişi yapmasını 
+sağlar bu da kullanıcı deneyimini geliştirir ve sayfa performansını artırır.
+
+*/
+
+/* AJAX NASIL ÇALIŞIR? 
+
+Çalışma İşlem Adımları :
+
+1-Olay meydana gelir.
+
+2-XMLHttpRequest Nesnesi Oluşturulur.
+
+3-XMLHttpRequest İstek Gönderilir.
+
+4-Sunucu isteği işler.
+
+5-Sunucu web sayfasına bir yanıt gönderir.
+
+6-Yanıt JS tarafından okunur.
+
+7-Uygun eylem JS tarafından gerçekleştirilir.
+
+
+*/
