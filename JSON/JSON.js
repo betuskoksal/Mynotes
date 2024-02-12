75b5c7f98677e226c9df577cb96511eebb174cd7
betuskoksal <koksal.betus@gmail.com>
2024-02-12 23:46:18 +0300

    update

---
+++ JSON/JSON.js
@@ -0,0 +1,18 @@
+/* JSON  (JavaScript Object Notation) NEDİR? 
+
+-Javascript nesne gösterimi de denebilir.
+
+-Javascript uygulamaları için oluşturulmuş bir veri formatıdır.
+
+-Veri transferlerinde verilerin XML'den daha az yer kaplamasını sağlar.
+
+-Veri iletimi için hafif, metin tabanlı bir veri formatıdır. JSON, anahtar-değer 
+çiftleri ve dizileri destekleyen bir veri yapısını temsil eder. 
+
+-Biçimi yalnızca bir metin olduğu için bir sunucu tarafından kolayca gönderilebilir 
+ve herhangi bir programlama diliyle bir veri biçimi olarak kullanılabilir.
+
+-Yani verilerimizi en düşük boyutta karşı tarafa çok kolay bir şekilde gönderebiliriz.
+
+-Bir sunucudaki verileri JSON biçiminde alırız ve onu başka JS nesne gibi kullanabiliriz.
+*/
